f097c467ea4e27cf1342382961a5c70f51b703da
Andres Perez <andresperezf@yahoo.com>
2023-01-26 09:53:53 -0500

    Updates to code

---
+++ force-apps/HOW/main/default/lwc/ex05ApexConfiguration/ex05ApexConfiguration.js
@@ -0,0 +1,113 @@
+import { api, LightningElement, track } from "lwc";
+
+export default class Ex05ApexConfiguration extends LightningElement {
+	_inputVariables;
+	_builderContext;
+	@track errors = {
+		hasErrors: false,
+		errorMessages: new Set(),
+		get messages() {
+			return Array.from(this.errorMessages).join(', ');
+		}
+	};
+
+	@track families = {
+		value: null,
+		options: []
+	};
+	@track countFamilies = {
+		min: 1,
+		max: 200,
+		value: 100
+	};
+	@track countContacts = {
+		min: 1,
+		max: 20,
+		value: 15
+	};
+
+	@api
+	get inputVariables() {
+		return this._inputVariables;
+	}
+	set inputVariables(value) {
+		if (value) {
+			this._inputVariables = value;
+			console.log(this._inputVariables);
+			this.countFamilies.value = this.inputVariables.find(({ name }) => name === "countFamilies")?.value;
+			this.countContacts.value = this.inputVariables.find(({ name }) => name === "countContacts")?.value;
+			let families = this.inputVariables.find(({ name }) => name === "families")?.value;
+			if (families) {
+				this.families.value = families;
+			} else {
+				this.families.value = "";
+			}
+		}
+	}
+
+	@api
+	get builderContext() {
+		return this._builderContext;
+	}
+	set builderContext(value) {
+		if (value) {
+			this._builderContext = value;
+			console.log(this._builderContext);
+			this.families.options = this._builderContext.recordLookups.map((rl) => ({
+				value: rl.name,
+				label: rl.label,
+				description: rl.description
+			}));
+			if (this.families.value === null) {
+				this.families.value = this.families.options[0].value;
+			}
+			this.families.options.unshift({ value: "", label: "None", description: "Blank" });
+		}
+	}
+
+	@api
+	validate() {
+		let output = [];
+		this.errors.hasErrors = true;
+		if (!this.families.value) {
+			this.errors.hasErrors = true;
+			let errorMessage = "Family is a required field. Select a value";
+			output.push(errorMessage);
+			this.errors.errorMessages.add(errorMessage);
+		}
+		return output;
+	}
+
+	onfamiliesChange(event) {
+		let newValue = event?.detail?.value;
+		this.families.value = newValue;
+		this.handleChange(newValue, "families", "reference");
+	}
+
+	onCountFamiliesChange(event) {
+		let newValue = Number(event?.detail?.value);
+		this.countFamilies.value = newValue;
+		this.handleChange(newValue, "countFamilies", "Number");
+	}
+
+	onCountContactsChange(event) {
+		let newValue = Number(event?.detail?.value);
+		this.countContacts.value = newValue;
+		this.handleChange(newValue, "countContacts", "Number");
+	}
+
+	handleChange(newValue, varName, varType) {
+		this.dispatchEvent(
+			new CustomEvent("configuration_editor_input_value_changed", {
+				bubbles: true,
+				cancelable: false,
+				composed: true,
+				detail: {
+					name: varName,
+					newValue,
+					newValueDataType: varType
+				}
+			})
+		);
+	}
+}
---
+++ force-apps/HOW/main/default/lwc/ex05LwcConfiguration/ex05LwcConfiguration.js
@@ -0,0 +1,69 @@
+import { api, LightningElement, track } from "lwc";
+
+export default class Ex05LwcConfiguration extends LightningElement {
+	_inputVariables;
+	_builderContext;
+
+	@track familyTypes = {
+		value: null,
+		options: []
+	};
+
+	@api
+	get inputVariables() {
+		return this._inputVariables;
+	}
+	set inputVariables(value) {
+		if (value) {
+			this._inputVariables = value;
+			console.log(this._inputVariables);
+			debugger;
+			let familyTypes = this.inputVariables.find(({ name }) => name === "familyTypes")?.value;
+			if (familyTypes) {
+				this.familyTypes.value = familyTypes;
+			} else {
+				this.familyTypes.value = "";
+			}
+		}
+	}
+
+	@api
+	get builderContext() {
+		return this._builderContext;
+	}
+	set builderContext(value) {
+		if (value) {
+			this._builderContext = value;
+			console.log(this._builderContext);
+			debugger;
+			this.familyTypes.options = this._builderContext.actionCalls.map((rl) => ({
+				value: `${rl.name}.response`,
+				label: rl.label,
+				description: rl.description
+			}));
+			this.familyTypes.value = this.familyTypes.options[0].value;
+			this.familyTypes.options.unshift({ value: "", label: "None", description: "Blank" });
+		}
+	}
+
+	onfamilyTypesChange(event) {
+		let newValue = event?.detail?.value;
+		this.familyTypes.value = newValue;
+		this.handleChange(newValue, "familyTypes", "reference");
+	}
+
+	handleChange(newValue, varName, varType) {
+		this.dispatchEvent(
+			new CustomEvent("configuration_editor_input_value_changed", {
+				bubbles: true,
+				cancelable: false,
+				composed: true,
+				detail: {
+					name: varName,
+					newValue,
+					newValueDataType: varType
+				}
+			})
+		);
+	}
+}
