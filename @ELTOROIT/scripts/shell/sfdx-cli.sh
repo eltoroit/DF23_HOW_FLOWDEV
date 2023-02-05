5970c3f6af5b52bf814aa6d3bc5a300a5209398a
Andres Perez <andresperezf@yahoo.com>
2023-02-05 07:21:57 -0500

    Scripts to generate scratch org

---
+++ @ELTOROIT/scripts/shell/sfdx-cli.sh
@@ -0,0 +1,31 @@
+echo "--- Creating org"
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDEMO -d 1
+
+echo "--- Opening org"
+sfdx force:org:open --path=/lightning/setup/DeployStatus/home
+
+echo "--- Displaying user"
+sfdx force:user:display --json
+
+echo "--- Deploying metadata"
+sfdx force:source:push --forceoverwrite --json
+
+echo "--- Assign permission set"
+sfdx force:user:permset:assign --permsetname HOW --json
+
+echo "--- Initializing user"
+sfdx force:apex:execute -f ./@ELTOROIT/scripts/shell/../apex/SetUserRecord.apex --json >> etLogs/apexAfterPush.json
+
+echo "--- Deploying admin user"
+mv .forceignore etLogs/.forceignore
+sfdx force:source:deploy -p force-apps/doNotDeploy/main/default/profiles/Admin.profile-meta.xml
+mv etLogs/.forceignore .forceignore
+
+echo "--- Importing data"
+sfdx ETCopyData:import -c ./@ELTOROIT/data --loglevel info --json --orgsource=soDEMO --orgdestination=soDEMO
+
+echo "--- Generating password"
+sfdx force:user:password:generate --json
+
+echo "--- Displaying user"
+sfdx force:user:display --json
